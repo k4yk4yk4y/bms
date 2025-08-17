cfd3355223f239ecea8b082d183ad0f091f87f9a
k4yk4yk4y <pasharusakevich2002@gmail.com>
2025-08-17 20:16:47 +0300

    Fix Rails test fixtures and controller tests
    - Updated marketing_requests.yml fixture with required fields
      * Added manager, partner_email, stag, request_type fields
      * Set proper values to satisfy model validations
    - Fixed marketing controller test URLs to match Rails routes
      * Corrected URL helper names (new_marketing_url, marketing_url(id), etc.)
      * Updated test logic for proper CRUD operations
      * Added proper parameters for create/update tests
    - All Rails tests now pass (8 tests, 8 assertions)
    - All RSpec tests still passing (633 examples)

--- test/controllers/marketing_controller_test.rb
+++ test/controllers/marketing_controller_test.rb
@@ -7,32 +7,44 @@ class MarketingControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get show" do
-    get marketing_show_url
+    marketing_request = marketing_requests(:one)
+    get marketing_url(marketing_request)
     assert_response :success
   end
 
   test "should get new" do
-    get marketing_new_url
+    get new_marketing_url
     assert_response :success
   end
 
   test "should get create" do
-    get marketing_create_url
-    assert_response :success
+    post marketing_index_url, params: { marketing_request: { 
+      manager: "Test Manager", 
+      partner_email: "test@example.com", 
+      promo_code: "TEST123", 
+      stag: "test_stag_unique", 
+      request_type: "promo_webs_50" 
+    } }
+    assert_response :redirect
   end
 
   test "should get edit" do
-    get marketing_edit_url
+    marketing_request = marketing_requests(:one)
+    get edit_marketing_url(marketing_request)
     assert_response :success
   end
 
   test "should get update" do
-    get marketing_update_url
-    assert_response :success
+    marketing_request = marketing_requests(:one)
+    patch marketing_url(marketing_request), params: { marketing_request: { 
+      manager: "Updated Manager" 
+    } }
+    assert_response :redirect
   end
 
   test "should get destroy" do
-    get marketing_destroy_url
-    assert_response :success
+    marketing_request = marketing_requests(:one)
+    delete marketing_url(marketing_request)
+    assert_response :redirect
   end
 end
