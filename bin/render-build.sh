61f7302f2e0df418e826fc8364c69bf02f05892c
k4yk4yk4y <pasharusakevich2002@gmail.com>
2025-10-09 20:26:00 +0300

    Update RAILS_MASTER_KEY in render.yaml and enhance render-build.sh with database migration step

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,8 @@
-#!/pasha/bin/env bash#exit on error
-
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
