d9830006b11f6ffe4ac576c1301212b710ffb5ba
k4yk4yk4y <pasharusakevich2002@gmail.com>
2026-01-11 16:26:40 +0300

    Add seeding of deploy data in render-build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,3 +9,6 @@ bundle exec rails assets:clean
 # Create default admin users (will use environment variables if set, otherwise defaults)
 echo "Creating default admin users..."
 bundle exec rails admin:create_default
+
+echo "Seeding deploy data..."
+bundle exec rails seed:deploy_data
